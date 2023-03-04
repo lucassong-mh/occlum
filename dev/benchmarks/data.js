44a512bd557ecfc5d82fc705b076be4ff691c968
github-action-benchmark <github@users.noreply.github.com>
2023-03-05 07:16:02 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 386e968ccb08bdd763e1915eafc39d8fb9df2226

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677971353425,
+  "lastUpdate": 1677971762897,
   "repoUrl": "https://github.com/lucassong-mh/occlum",
   "entries": {
     "FIO Benchmark on AsyncSFS and JinDisk": [
@@ -144,6 +144,42 @@ window.BENCHMARK_DATA = {
             "value": 208,
             "unit": "MiB/s",
             "extra": "randread"
+          }
+        ]
+      }
+    ],
+    "Iperf3 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Zheng, Qi",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "386e968ccb08bdd763e1915eafc39d8fb9df2226",
+          "message": "[libos] Enable backtrace for memory allocation failure",
+          "timestamp": "2023-01-29T02:52:47Z",
+          "url": "https://github.com/lucassong-mh/occlum/commit/386e968ccb08bdd763e1915eafc39d8fb9df2226"
+        },
+        "date": 1677971762105,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 4172,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 4172,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
           }
         ]
       }
